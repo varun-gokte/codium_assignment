a2dde79a60aeaa6983265a1be4e0fea798961dc8
varun-gokte <vsgokte@gmail.com>
2024-01-15 14:04:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,9 +15,7 @@ const server = require("http").Server(app);
 
 app.use(
   cors({
-    origin: [
-      "https://65a4dc4b3ee3e105cefcbdea--super-crisp-86304b.netlify.app",
-    ],
+    origin: ["https://main--stellar-profiterole-61a26c.netlify.app"],
   })
 );
 
